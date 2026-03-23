365fcada66d44470655956e7c55519bc70a2df4a
Ashvin Rokade <ashvinrokade@gmail.com>
2026-03-23 15:27:48 +1300

    added google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Providers from "@/components/Providers";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const geistSans = Geist({
   subsets: ["latin"],
@@ -41,6 +42,9 @@ export default function RootLayout({
       >
         <Providers>{children}</Providers>
       </body>
+      {process.env.NEXT_PUBLIC_GA_ID && (
+        <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID} />
+      )}
     </html>
   );
 }
